d079e861df02e577e71bf291f8a631517539076c
Daniel Lindsköld <lindskold.daniel@gmail.com>
2024-12-13 20:30:54 +0100

    treshold change

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 return [
     'api_base_url' => 'https://www.elprisetjustnu.se/api/v1/prices/',
     'region' => 'SE3',
-    'price_threshold' => 5,
+    'price_threshold' => 4,
     'email_sender' => 'admin@lindskold.me',
 ];
