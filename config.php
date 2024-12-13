746915e17088ab65d52ada7d0c4234100832073a
Daniel Lindsköld <lindskold.daniel@gmail.com>
2024-12-13 20:21:08 +0100

    config

--- config.php
+++ config.php
@@ -4,6 +4,5 @@ return [
     'api_base_url' => 'https://www.elprisetjustnu.se/api/v1/prices/',
     'region' => 'SE3',
     'price_threshold' => 5,
-    'email_recipients' => ['lindskold.daniel@gmail.com', 'lindskold@live.se'],
     'email_sender' => 'admin@lindskold.me',
 ];
