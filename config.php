ec15867d918785fbbe5d1b44765d24bb5aa0bcd2
Daniel Lindsköld <lindskold.daniel@gmail.com>
2025-03-22 22:09:48 +0100

    new admin email

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ return [
     'api_base_url' => 'https://www.elprisetjustnu.se/api/v1/prices/',
     'region' => 'SE3',
     'price_threshold' => 3,
-    'email_sender' => 'admin@lindskold.me',
+    'email_sender' => 'admin@lindskold.net',
 ];
